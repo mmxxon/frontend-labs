c0f057927d6b4e5bbb4d0da401f18a85935ea729
Maksym Movchan <xon.dev@protonmail.com>
2021-03-28 16:28:24 +0300

    feat: Convert function

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,25 @@
-const testModules = require('./test-module');
-require('../css/app.css');
+// import testModules from './test-module';
+import '../css/app.css';
 
-/** ******** Your code here! *********** */
+// eslint-disable-next-line no-unused-vars
+function convert(userList) {
+  const formattedUserList = userList.map((person) => ({
+    gender: person.gender,
+    title: person.name.title,
+    full_name: `${person.name.first} ${person.name.last}`,
+    city: person.location.city,
+    state: person.location.state,
+    country: person.location.country,
+    postcode: person.location.postcode,
+    coordinates: person.location.coordinates,
+    timezone: person.location.timezone,
+    email: person.email,
+    b_date: person.dob.date,
+    age: person.dob.age,
+    phone: person.phone,
+    picture_large: person.picture.large,
+    picture_thumbnail: person.picture.thumbnail,
+  }));
 
-// eslint-disable-next-line no-console
-console.log(testModules.hello);
+  return formattedUserList;
+}
